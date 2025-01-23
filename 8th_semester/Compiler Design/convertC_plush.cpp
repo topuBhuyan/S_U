2fe04884b827e46c28efa6be4438d03b1f4186e2
Topu <113087685+topuBhuyan@users.noreply.github.com>
2025-01-23 22:59:48 +0600

    Add files via upload

---
+++ 8th_semester/Compiler Design/convertC_plush.cpp
@@ -0,0 +1,140 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool is_delimeter(char ch) {
+
+   if(ch==' ' || ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch==','
+
+   || ch==';' || ch=='(' || ch==')' || ch=='{' || ch=='}' || ch=='[' || ch==']') {
+
+    return true;
+   }
+
+       return false;
+}
+
+bool is_operator(char ch){
+
+    if(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='^'
+       || ch=='%' ){
+
+        return true;
+
+       }
+
+       return false;
+}
+
+
+bool valid_identifier(char* str){
+
+if(str[0] =='0' || str[0] =='1' || str[0] =='2' || str[0] =='3' || str[0] =='4' ||
+   str[0] =='5' || str[0] =='6' || str[0] =='7' || str[0] =='8' ||
+   str[0] =='9' || is_delimeter(str[0]) == true ){
+
+    return false;
+   }
+
+   return true;
+
+}
+
+bool isKeword(char* str){
+
+if(!strcmp(str,"if") || !strcmp(str, "else")){
+
+    return true;
+}
+
+return false;
+
+}
+
+
+bool isKeyword(char* str) {
+    if (!strcmp(str, "auto") || !strcmp(str, "break") || !strcmp(str, "case") ||
+        !strcmp(str, "char") || !strcmp(str, "const") || !strcmp(str, "continue") ||
+        !strcmp(str, "default") || !strcmp(str, "do") || !strcmp(str, "double") ||
+        !strcmp(str, "else") || !strcmp(str, "enum") || !strcmp(str, "extern") ||
+        !strcmp(str, "float") || !strcmp(str, "for") || !strcmp(str, "goto") ||
+        !strcmp(str, "if") || !strcmp(str, "int") || !strcmp(str, "long") ||
+        !strcmp(str, "register") || !strcmp(str, "return") || !strcmp(str, "short") ||
+        !strcmp(str, "signed") || !strcmp(str, "sizeof") || !strcmp(str, "static") ||
+        !strcmp(str, "struct") || !strcmp(str, "switch") || !strcmp(str, "typedef") ||
+        !strcmp(str, "union") || !strcmp(str, "unsigned") || !strcmp(str, "void") ||
+        !strcmp(str, "volatile") || !strcmp(str, "while")) {
+        return true;
+    }
+    return false;
+}
+
+
+bool is_interger (char* str) {
+    int i = 0, len = strlen(str);
+
+    if(len == 0) {
+        return false;
+    }
+    if(str[0] == '-') {
+    i = 1;
+}
+for(; i<len; i++) {
+
+    if(str[i] != '0' && str[i] != '1' && str[i] != '2' && str[i] != '3' && str[i] != '4' && str[i] != '5' &&
+str[i] != '6'
+       && str[i] != '7' && str[i] != '8' && str[i] != '9') {
+        return false;
+       }
+    }
+    return true;
+}
+
+
+bool is_real_number(char* str) {
+
+    int i = 0, len = strlen(str);
+    bool has_decimal = false;
+
+    if(str[0] == '-') {
+        i = 1;
+    }
+
+    for(; i <len; i++) {
+
+        if(str[i] != '0' && str[i] != '1' &&str[i] != '2' && str[i] != '3' && str[i] != '4' && str[i] != '5' && str[i] != '6' &&
+           str[i] != '7' && str[i] != '8' && str[i] != '9' && str[i] != '.') {
+
+            return false;
+           }
+           if(str[i] == '.') {
+            has_decimal = true;
+           }
+    }
+    return has_decimal;
+}
+
+
+
+int main() {
+
+    char str[100];
+
+    int x = is_real_number ("-4.2");
+    cout << x;
+
+
+//    int v = valid_identifier("a");
+//    cout << v;
+
+//    int x = is_delimeter ('+');
+//    cout << x;
+
+//    int op = is_operator ('%');
+//    cout << op;
+
+
+
+    return 0;
+
+}
