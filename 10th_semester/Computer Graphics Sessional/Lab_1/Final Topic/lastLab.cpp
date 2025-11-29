9e67c9ce63cd7450383e7051b7f0c243e8c3d6b2
Topu <113087685+topuBhuyan@users.noreply.github.com>
2025-11-29 09:00:49 +0600

    Update lastLab.cpp

--- 10th_semester/Computer Graphics Sessional/Lab_1/Final Topic/lastLab.cpp
+++ 10th_semester/Computer Graphics Sessional/Lab_1/Final Topic/lastLab.cpp
@@ -29,23 +29,3 @@
     closegraph();
     return 0;
 }
-
-
-int gd = DETECT, gm;
-initgraph(&gd, &gm, "");
-
-rectangle(100, 100, 300, 300);
-line(130, 100, 130, 300);
-line(270, 100, 300);
-
-int cx = 200, cy = 200;
-int R = 50;
-circle(cx, cy, R);
-
-line(130, 200, 150, 200);
-line(250, 200, 270, 200);
-
-outtextxy(75, 80,"A(5,5)");
-outtextxy(120, 80, "C(10, 5)");
-outtextxy(270, 80, "E");
-outtextxy(280, 200, "H";)
