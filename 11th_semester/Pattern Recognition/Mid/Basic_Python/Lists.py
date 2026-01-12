08fe0b24c2e566e299b62a8dc29a64d8c61a7823
Topu <113087685+topuBhuyan@users.noreply.github.com>
2026-01-12 14:30:04 +0600

    Update Lists.py

--- 11th_semester/Pattern Recognition/Mid/Basic_Python/Lists.py
+++ 11th_semester/Pattern Recognition/Mid/Basic_Python/Lists.py
@@ -17,5 +17,50 @@ list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
 
 
+
 mylist = ["apple", "banana", "cherry"]
 print(type(mylist))
+
+print("------Access List Items--------")
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1]) # output: banana
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+print("------Change List Items-------")
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+print("--------Sort Lists--------")
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#Sort the list descending:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+
+
+"""
+##  List of Methods
+
+Method	        Description
+append()	Adds an element at the end of the list
+clear() 	Removes all the elements from the list
+copy()	    Returns a copy of the list
+count()	    Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	    Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	    Removes the element at the specified position
+remove()	Removes the item with the specified value
+reverse()	Reverses the order of the list
+sort()	    Sorts the list
+
+"""
+
